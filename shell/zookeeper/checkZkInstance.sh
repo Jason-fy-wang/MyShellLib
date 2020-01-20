2a5d7d32c24aacf21743227dd13f245f9b3b0a76
emperwang <544094478@qq.com>
2020-01-20 11:29:18 +0800

    zkInstance check

---
+++ shell/zookeeper/checkZkInstance.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+BASE=$(cd $(dirname $0);pwd)
+
+zkCLi=/opt/zookeeper-3.4.14/bin/zkServer.sh
+checkInstance(){
+   id=$(ps -ef | grep -v "grep" | grep 'QuorumPeerMain' | awk '{print $2}')
+   id=(id)
+   if [ "${id[*]}" -le 0 ];then
+        echo "1"
+        exit 1
+   fi
+
+}
+
+checkStatus(){
+    $(${zkCLi}  status 2>/dev/null | grep -E "[leader|follower]") >/dev/null 2>&1
+    if [ "$?" -gt 0 ]; then
+        echo "1"
+        exit 1
+    fi
+}
+
+main(){
+checkInstance
+checkStatus
+echo "0"
+}
+
+main
