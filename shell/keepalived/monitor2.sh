42fb14356c6ce34eda31352c23e997d295778671
emperwang <544094478@qq.com>
2020-07-03 11:35:27 +0800

    fix

--- shell/keepalived/monitor2.sh
+++ shell/keepalived/monitor2.sh
@@ -8,9 +8,9 @@ Key=virtual_ipaddress
 Key2=unicast_peer
 VIP=$(sed -n -e "/${Key}/,+1p"  ${FILE} | grep -v  "${Key}" | awk '{print $1}')
 PIP=$(sed -n "/${Key2}/,+1p" /etc/keepalived/keepalived.conf | grep -v "${Key2}" | awk '{print $1'})
-$(ping -c 1 -t 1 $VIP >>/dev/null 2>&1) 
+$(ping -c 1 -t 1 $VIP >/dev/null 2>&1) 
 RES=$?
-TCHECK=$(ps -ef | grep -v grep | grep keepalived >>/dev/null 2>&1)
+TCHECK=$(ps -ef | grep -v grep | grep keepalived >/dev/null 2>&1)
 
 if [ "$RES" -eq 0 ];then
     echo 0
