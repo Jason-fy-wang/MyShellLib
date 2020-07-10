cb6d156e1130367ab373bb2d5a2e90e454adda7e
emperwang <544094478@qq.com>
2020-07-10 16:01:18 +0800

    manage filter:

--- shell/postgresqlDB/filter_strategy-upgrade/manager_am_filter_strategy.sh
+++ shell/postgresqlDB/filter_strategy-upgrade/manager_am_filter_strategy.sh
@@ -16,22 +16,30 @@ usage(){
 }
 
 insert(){
-    echo 'input value: source_id  alarm_type  orig_serverity(critical,major,minor,warning,null)  object_type object_uid  start_time end_time (time format:"2020-01-01 00:00:00")'
-    read type value istime ietime
-    echo "type=${type}, value=${value},isTime = ${istime}, ietime=${ietime}"
-    #echo "${SrcConn}"
+    echo 'input value: source_id  alarm_type  orig_serverity(critical,major,minor,warning,null)  object_type object_uid  start_time end_time (time format:"2020-01-01\ 00:00:00")'
+    read isid ialarmtype iorigserver iobjtype iobjuid istime ietime
+    #echo "source_id=${isid}, alarm_type=${ialarmtype},orig_serverity=${iorigserver},object_type=${iobjtype},object_uid=${iobjuid},start_time=${istime},end_time=${ietime}"
+    if [ "${iorigserver}" != "critical" ] && [ "${iorigserver}" != "major" ] && [ "${iorigserver}" != "minor" ] && [ "${iorigserver}" != "warning" ] && [ "${iorigserver}" != "null" ] ; then
+        echo "orig_serverity should in (critical,major,minor,warning,null)"
+        return
+    fi
+    OIFS="${IFS}"
+    IFS="#"
     dtime=$(date +"%F %T")
+    iargs=($isid#$ialarmtype#$iorigserver#$iobjtype#$iobjuid#$istime#$ietime#$dtime)
     sql="insert into filter_strategy_upgrade(source_id,alarm_type,orig_serverity,object_type,object_uid,start_time,end_time,update_time) values('"
-    sql="${sql}$type',"
-    sql="$sql'"
-    sql="$sql$value',"
-    sql="$sql'"
-    sql="${sql}${istime}',"
-    sql="$sql'"
-    sql="${sql}${ietime}',"
-    sql="$sql'"
-    sql="$sql $dtime');"
-    psql "${SrcConn}" -c "$sql"
+    for i in ${iargs[@]}
+    do
+        if [ "$i" == "${dtime}" ]; then
+            sql="${sql}$i'"
+        else 
+            sql="${sql}$i',"
+            sql="$sql'"
+        fi
+    done
+    #echo "sql = $sql"
+    IFS=${OIFS}
+    psql "${SrcConn}" -c "${sql});"
 }
 getdata(){
     echo "**********************"
@@ -81,17 +89,28 @@ deldata(){
 }
 
 update(){
-    echo 'update with id, Input value: auto_id(primary key) source_id alarm_type orig_serverity object_type object_uid start_time end_time (time format:"2020-01-01 00:00:00")'
-    read uid ttu valu stime etime
+    echo 'update with id, Input value: auto_id(primary key) source_id  alarm_type  orig_serverity(critical,major,minor,warning,null)  object_type object_uid  start_time end_time (time format:"2020-01-01\ 00:00:00")'
+    read uaid usid ualarmtype uorigserver uobjtype uobjuid ustime uetime
+    #echo "auto_id=${uaid},source_id=${usid},alarm_type=${ualarmtype},orig_serverity=${uorigserver},object_type=${uobjtype},object_uid=${uobjuid},start_time=${ustime},end_time=${uetime}"
+    if [ "${uorigserver}" != "critical" ] && [ "${uorigserver}" != "major" ] && [ "${uorigserver}" != "minor" ] && [ "${uorigserver}" != "warning" ] && [ "${uorigserver}" != "null" ] ; then
+        echo "orig_serverity should in (critical,major,minor,warning,null)"
+        return
+    fi
+    OIFS="${IFS}"
+    IFS="#"
     dtimeu=$(date +"%F %T")
-    sqlu="update filter_strategy_upgrade set update_time='"
-    sqlu="${sqlu}${dtimeu}', type='"
-    sqlu="${sqlu}$ttu',value='"
-    sqlu="${sqlu}${valu}',start_time='"
-    sqlu="${sqlu}${stime}',end_time='"
-    sqlu="${sqlu}${etime}' where auto_id='"
-    sqlu="${sqlu}${uid}';"
-    psql "${SrcConn}" -c  "$sqlu"
+    uargs=(source_id=\'${usid}\'#alarm_type=\'${ualarmtype}\'#orig_serverity=\'${uorigserver}\'#object_type=\'${uobjtype}\'#object_uid=\'${uobjuid}\'#start_time=\'${ustime}\'#end_time=\'${uetime}\')
+    leth=${#uargs[@]}
+    sqlu="update filter_strategy_upgrade set "
+    for i in ${uargs[@]}
+    do
+            sqlu="${sqlu}${i},"
+    done
+    sqlu="${sqlu}update_time='"
+    sqlu="${sqlu}${dtimeu}'"
+    #echo "sqlu = ${sqlu}"
+    IFS=${OIFS}
+    psql "${SrcConn}" -c  "${sqlu} where auto_id=${uaid} ;"
 }
 
 process(){
@@ -131,7 +150,7 @@ main(){
         s)pwd=$OPTARG;;
         d)db=$OPTARG;;
         ?) usage 
-         exit 1    
+            exit 1    
         ;;
         esac
     done
