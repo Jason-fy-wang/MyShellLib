9f3d1db52eda11c14fb9e7c9d788f3d6e58e1d82
emperwang <544094478@qq.com>
2020-07-22 19:36:22 +0800

    fix script

--- shell/keepalived/publish/check_rem_sms_agent_status-bak.sh
+++ shell/keepalived/publish/check_rem_sms_agent_status-bak.sh
@@ -23,7 +23,7 @@ if [ 0 -eq "$alived" ]; then
     if [ 1 -eq "$rst" ]; then
         echo 1
         #echo "start app"
-        ${scpath/bin/start.sh start} > /dev/null 2>&1
+        ${scpath}/start.sh start > /dev/null 2>&1
         exit 1
     fi
     echo 0
@@ -31,7 +31,7 @@ else
     if [ 0 -eq "$rst" ];then
         echo 1
         #echo "stop app"
-        ${scpath/bin/start.sh stop} > /dev/null 2>&1
+        ${scpath}/start.sh stop > /dev/null 2>&1
         exit 1
     fi
     echo 0
