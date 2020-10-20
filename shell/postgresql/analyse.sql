3e737d1e4c61e2af7ec0cde7e749bd66da11248d
kai.wang <kai.l.wang@ericsson.com>
2020-10-20 18:02:40 +0800

    postgresql cmd

--- shell/postgresql/analyse.sql
+++ shell/postgresql/analyse.sql
@@ -45,3 +45,60 @@ select statistical_type,sum(amount) amount from alarm_processing_flow where time
 
 select statistical_type, amount, round(cast(amount as numeric)/ cast((select sum(amount) amount from alarm_processing_flow where time_point_stamp>='2020-09-21 13:00:00' and time_point_stamp<='2020-09-22 13:00:00' and  statistical_type='collectedAlarm')as numeric),2)rate from (
 select statistical_type,sum(amount) amount from alarm_processing_flow where time_point_stamp>='2020-09-21 13:00:00' and time_point_stamp<='2020-09-22 13:00:00' group by statistical_type) b ;
+
+
+
+
+---------------------excel 映射字段添加
+insert into field_enum_value(field,cn_name,value) values
+('AlarmFieldName','告警唯一标识','alarmId'),
+('AlarmFieldName','告警标题','alarmTitle'),
+('AlarmFieldName','告警状态','clearFlag'),          -- no
+('AlarmFieldName','是否根告警','ifRoot'),
+('AlarmFieldName','是否主告警','ifMaster'),
+('AlarmFieldName','是否从告警','ifSlave'),
+('AlarmFieldName','是否本大区产生','localProducer'),
+('AlarmFieldName','是否归属于本大区','localOwner'),     -- no 之间都没有
+('AlarmFieldName','告警对象名称','objectName'),
+('AlarmFieldName','告警对象类型','objectType'),
+('AlarmFieldName','告警类型','alarmType'),
+('AlarmFieldName','原始告警级别','origSeverity'),
+('AlarmFieldName','网管告警级别','netManagerAlarmSeverity'),    --n
+('AlarmFieldName','告警数据源','dataSource'),
+('AlarmFieldName','设备厂家','vendorName'),
+('AlarmFieldName','告警发生时间','eventTime'),
+('AlarmFieldName','告警最晚上报时间','latestArriveTime'),   -- n
+('AlarmFieldName','原始的告警ID','initialAlarmId'),     -- n
+('AlarmFieldName','告警信息序号','alarmSeq'),
+('AlarmFieldName','告警问题原因Id','specificProblemID'),
+('AlarmFieldName','告警问题原因','specificProblem'),
+('AlarmFieldName','告警对象UID','objectUID'),
+('AlarmFieldName','告警定位对象UID','subObjectUID'),
+('AlarmFieldName','告警定位对象名称','subObjectName'),
+('AlarmFieldName','告警定位对象资源类型','subObjectType'),
+('AlarmFieldName','告警定位信息','locationInfo'),
+('AlarmFieldName','告警辅助信息','addInfo'),
+('AlarmFieldName','告警设备的虚实性','pVFlag'),
+('AlarmFieldName','标准化结果','standardFlag'),
+('AlarmFieldName','是否已确认','confirmFlag'),
+('AlarmFieldName','告警专业类型','specialty'),
+('AlarmFieldName','告警涉及的vmId列表','vmIdList'),
+('AlarmFieldName','告警涉及的HostId列表','hostIdList'),
+('AlarmFieldName','网管告警ID','netManagerAlarmId'),
+('AlarmFieldName','业务类型','businessType'),
+('AlarmFieldName','告警清除人','clearOperateUser'),
+('AlarmFieldName','告警清除时间','clearTime'),
+('AlarmFieldName','归属域 （CT/IT）','domainType'),
+('AlarmFieldName','告警源ID','sourceID'),
+('AlarmFieldName','告警是否过滤','isFilter'),
+('AlarmFieldName','是否工程告警','pFlag'),
+('AlarmFieldName','S-NSSAI列表','snssaiList'),
+('AlarmFieldName','NSSI列表','nssiList'),
+('AlarmFieldName','NSSI名称列表','nssiNameList'),
+
+
+('DataSourceType','虚拟资源','VIM'),
+('DataSourceType','物理资源','PIM'),
+('DataSourceType','VNF业务','EMS'),
+('DataSourceType','sm','SM'),
+('DataSourceType','未匹配则显示数据本身','PM');
---
+++ shell/postgresql/postgresql-sql.sql
@@ -0,0 +1,99 @@
+-- 1. 查看所有数据库
+\list
+select * from pg_database;
+-- 2. 连接数据库 
+\c  databasename;
+\c - user;      --  切换用户
+-- 3. list tables
+\db
+
+-- 4. 查找帮助
+\?
+\h create schema   -- 查看创建schema的帮助
+
+fcapsdb=# \h create schema;
+命令：       CREATE SCHEMA
+描述：       建立新的架构模式
+语法：
+CREATE SCHEMA 模式名称 [ AUTHORIZATION role_specification ] [ 模式中对象 [ ... ] ]
+CREATE SCHEMA AUTHORIZATION role_specification [ 模式中对象 [ ... ] ]
+CREATE SCHEMA IF NOT EXISTS 模式名称 [ AUTHORIZATION role_specification ]
+CREATE SCHEMA IF NOT EXISTS AUTHORIZATION role_specification
+
+这里role_specification可以是：
+
+    用户名
+  | CURRENT_USER
+  | SESSION_USER
+
+-- 5. 创建schema
+create  schema schemaname;
+\dn    --  查看所有schema
+drop schema [if exists] schemaname;
+
+-- 查看当前schema
+select current_schema;
+
+-- 查看search_path
+show search_path;
+
+-- 修改schema的所属用户
+alter schema schname owner to username;
+-- 设置schema表空间
+mkdir -p /data/pg_sql/tbs_space;
+create tablespace tbs_space owner schname localtion '/data/pg_sql/tbs_space';
+
+-- 创建数据库并制定所有者
+create database dbname with owner username encoding UTF8 template template1 tablespace tbs_space;
+
+-- 6. 创建用户
+create user fcafm;
+create user fm with password '123456';
+create schema fm_db authorization 'fm';
+alter user fm set search_path='fm';
+
+-- 修改用户密码
+alter user fcafm password '123456';
+-- 修改用户权限
+alter user fcafm createrole createdb replication login;
+
+-- 修改search_path
+alter user fcafm set search_path='fm','$user';
+
+-- 7. 数据备份
+pg_dump -h 10.163.119.68 -p 5432 -U user --schema=ff --table=tbl -d dbname > bak.sql
+pg_restore -h 10.163.119.68 -p 5432 -U user --schema=ff   --table=tbl -d dbname < bak.sql
+
+-- 8 jsonb 查询
+-- 从slarm中查找ala_tent(jsonb字段)包含{"pFlag":"Yes"}内容的 Flag字段的值
+select ala_tent->'Flag' from slarm where ala_tent @> '{"pFlag":"Yes"}'
+
+
+-- 9. 模糊查找
+-- like,not like 用于模糊查询,其中%表示任意个字符, _ 表示单个任意字符,如果需要在模糊查询中查询这两个通配符,需要使用
+-- ESCAPE 进行转义
+select * from tabname where name like 'zh/_%' ESCAPE '/';
+
+-- postgresql 中特殊的操作符:ilike, not ilike: 
+-- ilike: 表示在模糊匹配字符时不区分大小写, i 即 ignore
+-- not ilike: 表示不模糊匹配字符串 且 不区分大小写
+
+-- 10 正则查找
+-- ~   ~*  以及   !~ !~*
+-- ~  表示匹配正则表达式,且区分大小写
+-- ~* 表示匹配正则表达式,且不区分大小写
+-- !~  是·的否定用法,表示不匹配正则表达式,且区分大小写
+-- !~* 是~* 的否定用法,表示不匹配正则表达式,且不区分大小写
+-- ~~  等效于 like
+-- ~~* 等效于 ilike
+-- !~~ 等效于 not like
+-- !~~* 等效于not ilike
+
+-- 匹配以 "张" 开头的字符串
+select * from  table where name ~ '^张';
+-- 匹配以 "小" 结尾的字符串
+select *  from table where name ~ '小$';
+
+
+
+
